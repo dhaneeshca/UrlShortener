e6000583a38f24eefb301f11d69c8141a3c660e0
Dhaneeshvp <dhaneesh.vp@you.co>
2025-03-17 13:18:06 +0530

    custom domain with url expiry logic

--- routes/url_handler.go
+++ routes/url_handler.go
@@ -6,14 +6,17 @@ import (
 	"errors"
 	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	"gorm.io/gorm"
 )
 
 type ShortenRequest struct {
-	LongURL  string `json:"long_url"`
-	ShortURL string `json:"short_url,omitempty"` // Optional custom short URL
+	LongURL      string `json:"long_url"`
+	ShortURL     string `json:"short_url,omitempty"`
+	CustomDomain string `json:"custom_domain,omitempty"`
+	ExpiryDays   int32  `json:"expiry_days,omitempty"`
 }
 
 type ShortenResponse struct {
@@ -28,6 +31,8 @@ func generateShortCode() string {
 	}
 	return string(shortCode)
 }
+
+const expiration_days_const = 1000
 
 // Handler for shortening URLs
 func ShortenURL(db *gorm.DB) http.HandlerFunc {
@@ -45,9 +50,20 @@ func ShortenURL(db *gorm.DB) http.HandlerFunc {
 			shortCode = generateShortCode()
 		}
 
+		//custom domain is for storage only as the redirectiopn will be done by the domain owner,
+		// we will handle the redirection part alone
+		customDomain := req.CustomDomain
+
+		// add expiry date
+		expiration_days := req.ExpiryDays
+		if expiration_days == 0 {
+			expiration_days = expiration_days_const
+		}
+		expiry_date := time.Now().AddDate(0, 0, int(expiration_days))
+
 		// Check if the short URL already exists
 		existingURL := models.URL{}
-		if err := db.Where("short_url = ?", shortCode).First(&existingURL).Error; err == nil {
+		if err := db.Where("short_url = ? ", shortCode).First(&existingURL).Error; err == nil {
 			http.Error(w, "Short URL already taken", http.StatusConflict)
 			return
 		} else if err != gorm.ErrRecordNotFound {
@@ -56,14 +72,19 @@ func ShortenURL(db *gorm.DB) http.HandlerFunc {
 		}
 
 		// Save to database
-		url := models.URL{LongURL: req.LongURL, ShortURL: shortCode}
+		url := models.URL{LongURL: req.LongURL, ShortURL: shortCode, ExpiryDate: expiry_date, CustomDomain: customDomain}
 		if err := db.Create(&url).Error; err != nil {
 			http.Error(w, "Failed to save URL", http.StatusInternalServerError)
 			return
 		}
 
+		domain := "http://localhost:8080/"
+		if customDomain != "" {
+			domain = customDomain + "/"
+		}
+
 		// Response
-		resp := ShortenResponse{ShortURL: "http://localhost:8080/" + shortCode}
+		resp := ShortenResponse{ShortURL: domain + shortCode}
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(resp)
 	}
@@ -85,6 +106,10 @@ func RedirectDB(db *gorm.DB) http.HandlerFunc {
 			} else {
 				http.Error(w, "Database error", http.StatusInternalServerError)
 			}
+			return
+		}
+		if url.ExpiryDate.Before(time.Now()) {
+			http.Error(w, "Url Has expired", http.StatusNotFound)
 			return
 		}
 
